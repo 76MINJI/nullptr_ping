7af8583df6995e9bd283012c749c5690728dfb19
bangyewon <90364660+bangyewon@users.noreply.github.com>
2025-06-09 16:35:51 +0900

    [refactor] 이미 한번 재판 회부 누른 사용자만 경고창 보이게 수정

--- Otherdetailping.php
+++ Otherdetailping.php
@@ -207,14 +207,18 @@ if (count($reviews)===0) {
           $ins->bind_param("iii", $base_pkey, $excuse_pkey, $current_user_pkey);
           $ins->execute();
           $ins->close();
+
+          header("Location: Otherdetailping.php?id={$excuse_pkey}");
+          exit;
+      } else {
+          echo "<script>
+                  alert('재판 회부는 1회만 가능합니다.');
+                  location.href='Otherdetailping.php?id={$excuse_pkey}';
+                </script>";
+          exit;
       }
-      //TODO 이미 내가 재판회부 했을때만 뜨기 .. 수정
-      echo "<script>
-      alert('재판 회부는 1회만 가능합니다.');
-      location.href='Otherdetailping.php?id={$excuse_pkey}';a
-    </script>";
-      exit;
   }
+  
   
   
 
